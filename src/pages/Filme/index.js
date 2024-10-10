fe809a2b3f5d17a986636f79a40b40054df62cea
Carol <carolinaabpessoa@gmail.com>
2024-10-10 00:25:02 -0300

    developing movie details page

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,54 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import  api from '../../services/api';
+
+import './filme.css';
+
 function Filme() {
+    const { id } = useParams(); //pegando id passado como parametro na url 
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: "135fef96c7de1b86e0ac8f15d7cc0422",
+                    language: "pt-BR",
+                }
+            })
+            .then((response) => {
+                setFilme(response.data);
+                setLoading(false);
+            }) //caso de sucesso da promise (get) - achou o filme
+            .catch((error) => { // não achou o filme 
+                setLoading(false);
+            })
+        }
+
+        loadFilme();
+
+        return () => { //para quando o componente for "desmontado"
+
+        }
+    }, []);
+
+    if(loading) {
+        return(
+            <div className='filme-detail'>
+                <h2>Carregando detalhes do filme...</h2>
+            </div>
+        );
+    }
+
     return(
-        <div>
-            <h1>bem vindo aos filmes!</h1>
+        <div className='filme-detail'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}/>
+            <h3>Sinopse:</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average} / 10</strong>
         </div>
     )
 }
